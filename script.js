a139c9caabe538648c06803dd84fcd90aabf02bb
Martosky <imoniaromartins@yahoo.com>
2024-05-30 23:57:15 +0100

    js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let mesg = document.querySelector('.message');
 
 function averageNum(score){
+    let aveSco = 0
+
+    for (let i = 0; i < score.length; i++){
+        aveSco + score[i]
+    }
+    return aveSco / score.length
+
+    }
     
-}
+function getGrade(){}
