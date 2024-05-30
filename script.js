392ed624b5e5e5e592e15760b388471ae14d6d1b
Martosky <imoniaromartins@yahoo.com>
2024-05-31 00:05:09 +0100

    js

--- script.js
+++ script.js
@@ -14,5 +14,10 @@ function getGrade(score){
 
      if(score == 100){
         return "A++"
+     } else if (score >= 90){
+        return "A";
+     }else if (score >= 80){
+        return "B";
      }
+
 }
